e71ab9221f284cc6a9afb66afb6b7e8b04c8b2a5
Evirustandi <36563136+Evirustandi@users.noreply.github.com>
2026-04-21 11:54:18 +0700

    Update data.js

--- data/data.js
+++ data/data.js
@@ -7,32 +7,32 @@ var CMS_BERITA = [];
 var CMS_GALERI = [
   {
     "id": "mo8554lr1g5sapcwfgb",
-    "url": "foto/kelas 1.jpg",
+    "url": "https://drive.google.com/file/d/1uLX4d7MsnFfxJ0QI3Rk6tE1VIbkGWf-1/view?usp=drive_link",
     "caption": "Foto Bersama Kelas 1"
   },
   {
     "id": "mo8571nfxisandf1kh",
-    "url": "foto/kelas 2.jpg",
+    "url": "https://drive.google.com/file/d/15xjJXMqh9Ga0lTdy-pDZnfosL3MXXE2I/view?usp=sharing",
     "caption": "Foto Bersama Kelas 2"
   },
   {
     "id": "mo857q6l7sn5venyjp",
-    "url": "foto/kelas 3a.jpg",
+    "url": "https://drive.google.com/file/d/15VSEBklhVAox3wSf2HdQ6YkwVkSvqL2A/view?usp=drive_link",
     "caption": "Foto Bersama Kelas 3A"
   },
   {
     "id": "mo858dtp0n4ls2prfaoq",
-    "url": "foto/kelas 3b.jpg",
+    "url": "https://drive.google.com/file/d/1OdEwGx8nyuVpNU_y-Uak5ybLtXnR7kTW/view?usp=sharing",
     "caption": "Foto Bersama Kelas 3B"
   },
   {
     "id": "mo858pg3t9icbmd7ljq",
-    "url": "foto/kelas 4.jpg",
+    "url": "https://drive.google.com/file/d/1yjDNpJ2m0JzbEKUf0gZOSyWyybgULoFZ/view?usp=sharing",
     "caption": "Foto Bersama Kelas 4"
   },
   {
     "id": "mo8597kzfa53i1vc1sw",
-    "url": "foto/kelas 5.jpg",
+    "url": "https://drive.google.com/file/d/1XX1MneqKoj4VCq2n0fxUL2DeGMz4mUh9/view?usp=sharing",
     "caption": "Foto Bersama Kelas 5"
   }
 ];
