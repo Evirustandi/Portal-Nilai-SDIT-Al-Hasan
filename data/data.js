46c382ec714230d3cee82d6d0bcebdfae0ed324d
Evirustandi <36563136+Evirustandi@users.noreply.github.com>
2026-04-21 12:05:12 +0700

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -1,5 +1,5 @@
 // data.js — Di-generate otomatis oleh CMS Admin SDIT Al-Hasan
-// Tanggal export: 21/4/2026, 11.49.13
+// Tanggal export: 21/4/2026, 12.04.02
 // JANGAN edit file ini manual! Gunakan dashboard admin.
 
 var CMS_BERITA = [];
@@ -12,7 +12,7 @@ var CMS_GALERI = [
   },
   {
     "id": "mo8571nfxisandf1kh",
-    "url": "https://drive.google.com/file/d/15xjJXMqh9Ga0lTdy-pDZnfosL3MXXE2I/view?usp=sharing",
+    "url": "https://drive.google.com/file/d/15xjJXMqh9Ga0lTdy-pDZnfosL3MXXE2I/view?usp=drive_link",
     "caption": "Foto Bersama Kelas 2"
   },
   {
@@ -22,12 +22,12 @@ var CMS_GALERI = [
   },
   {
     "id": "mo858dtp0n4ls2prfaoq",
-    "url": "https://drive.google.com/file/d/1OdEwGx8nyuVpNU_y-Uak5ybLtXnR7kTW/view?usp=sharing",
+    "url": "https://drive.google.com/file/d/1OdEwGx8nyuVpNU_y-Uak5ybLtXnR7kTW/view?usp=drive_link",
     "caption": "Foto Bersama Kelas 3B"
   },
   {
     "id": "mo858pg3t9icbmd7ljq",
-    "url": "https://drive.google.com/file/d/1yjDNpJ2m0JzbEKUf0gZOSyWyybgULoFZ/view?usp=sharing",
+    "url": "https://drive.google.com/file/d/1yjDNpJ2m0JzbEKUf0gZOSyWyybgULoFZ/view?usp=drive_link",
     "caption": "Foto Bersama Kelas 4"
   },
   {
@@ -37,10 +37,209 @@ var CMS_GALERI = [
   }
 ];
 
-var CMS_GURU = [];
+var CMS_GURU = [
+  {
+    "id": "mo85ir5n5fz76pto7y",
+    "nama": "Rahmatuloh, S.Pd.I",
+    "jabatan": "Kepala Sekolah",
+    "inisial": "R",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85jaeb4vjhobnrwwf",
+    "nama": "Agisa Oktanadila, S.I.Pust",
+    "jabatan": "Bendahara Sekolah",
+    "inisial": "AO",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85joxnweb278z3r1",
+    "nama": "Muhammad Evirustandi, S.Kom",
+    "jabatan": "Operator Sekolah",
+    "inisial": "ME",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85k3w3plz4rnepr4",
+    "nama": "Bambam Faidul Manan",
+    "jabatan": "Administrasi",
+    "inisial": "BFM",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85khg79zn9x9dabv",
+    "nama": "Risvia Ningsih",
+    "jabatan": "Media Informasi",
+    "inisial": "RN",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85l01fqeciep6pfbk",
+    "nama": "Mega Sri Wahyuni, S.Pd",
+    "jabatan": "Guru Kelas 1A",
+    "inisial": "MSW",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85lhilo68k4atb2w",
+    "nama": "Dhea Fitria Ningsih, S.Pd.",
+    "jabatan": "Guru Kelas 1B",
+    "inisial": "DFN",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85m205111drahef0oo",
+    "nama": "Alfi Nur Fauziyah,S.Pd",
+    "jabatan": "Guru Kelas 2A",
+    "inisial": "ANF",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85mlydzo5xd2tcfm8",
+    "nama": "Rif'at Abdul Basir, S.Pd.",
+    "jabatan": "Guru Kelas 2B",
+    "inisial": "RAB",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85my6hz976faurys",
+    "nama": "Resti Febrianti",
+    "jabatan": "Guru Kelas 3A",
+    "inisial": "RF",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85nbhf04frw670xkqh",
+    "nama": "Susilawati, S.Pd.",
+    "jabatan": "Guru Kelas 3B",
+    "inisial": "S",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85nr6fa0k46qct676",
+    "nama": "Virda Amalia Amimi, S.H.",
+    "jabatan": "Guru Kelas 4A",
+    "inisial": "VAM",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85ogvjj01u1pc13zd",
+    "nama": "Anisa Cikal Destiani, S.Pd (Cand)",
+    "jabatan": "Guru Kelas 4B",
+    "inisial": "ACD",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85ouupgy8v5pi9r1b",
+    "nama": "Siti Hudaebiah,S.Pd.",
+    "jabatan": "Guru Kelas 5A",
+    "inisial": "SH",
+    "status": "Aktif",
+    "mapel": ""
+  },
+  {
+    "id": "mo85p91p9xxnqxw4ohq",
+    "nama": "Dewi Gandari Sukawati, S.Pd.",
+    "jabatan": "Guru Kelas 5B",
+    "inisial": "DGS",
+    "status": "Aktif",
+    "mapel": ""
+  }
+];
 
 var CMS_PROGRAM = [];
 
 var CMS_SPMB = [];
 
-var DATA_NILAI = [];
+var DATA_NILAI = [
+  {
+    "nisn": "12;3196590768;Fawwaz Almuazzam Wijaya;87",
+    "nama": "5;87",
+    "kelas": "5;94",
+    "tglLahir": "5;73;89;77;81",
+    "nilai": {
+      "sts1": [],
+      "sas1": [],
+      "sts2": [],
+      "sas2": [],
+      "5;87": [
+        {
+          "mapel": "P. Agama & BP",
+          "nilai": 5,
+          "kkm": 70
+        }
+      ]
+    }
+  },
+  {
+    "nisn": "16;3188693234;KHOERUNNISA SALSABILA FITRI;68",
+    "nama": "5;77",
+    "kelas": "5;82",
+    "tglLahir": "5;43;60",
+    "nilai": {
+      "sts1": [],
+      "sas1": [],
+      "sts2": [],
+      "sas2": [],
+      "5;79;77": [
+        {
+          "mapel": "P. Agama & BP",
+          "nilai": 5,
+          "kkm": 70
+        }
+      ]
+    }
+  },
+  {
+    "nisn": "17;3193471551;MAURA AYUNINGTIAS;97",
+    "nama": "5;92",
+    "kelas": "5;78",
+    "tglLahir": "5;56;74",
+    "nilai": {
+      "sts1": [],
+      "sas1": [],
+      "sts2": [],
+      "sas2": [],
+      "5;79;91;88;86": []
+    }
+  },
+  {
+    "nisn": "18;3181796195;MIKHAYLA NAUREEN IVANKA;87",
+    "nama": "5;92",
+    "kelas": "5;88;80;85",
+    "tglLahir": "5;75;99;84",
+    "nilai": {
+      "sts1": [],
+      "sas1": [],
+      "sts2": [],
+      "sas2": [],
+      "5;91": []
+    }
+  },
+  {
+    "nisn": "23;3180323204;Muhammad Khalid Danial Alhusain;92",
+    "nama": "5;100;90",
+    "kelas": "5;76;87",
+    "tglLahir": "5;91;100;76",
+    "nilai": {
+      "sts1": [],
+      "sas1": [],
+      "sts2": [],
+      "sas2": [],
+      "5;89": []
+    }
+  }
+];
